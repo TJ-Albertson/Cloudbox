7c4e88bf35158db45cbd57605aa621e5d481f5ce
TJ-Albertson <THOMASJOHNNY77@GMAIL.COM>
2022-06-30 15:33:36 -0500

    a

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,6 +2,17 @@ import React, { useState, useEffect } from 'react';
 import './CSS/NavBar.css'
 
 export default function NavBar(props) {
+
+    async function getProfileJSON() {
+        try {
+          let response = await fetch('http://localhost:5000/profile');
+          let responseJSON = await response.json();
+          return responseJSON;
+         } catch(error) {
+          console.error(error);
+        }
+      }
+
     return (
         <div>
 
@@ -19,6 +30,10 @@ export default function NavBar(props) {
 
                 <li className='NavRight'>
                     <a className='Link'>Settings</a>
+                </li>
+
+                <li className='NavRight'>
+                    <a className='Link'>{getProfileJSON().name}</a>
                 </li>
                 
             </ul>
