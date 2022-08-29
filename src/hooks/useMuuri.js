a6e075cfc358f0763695871e6271116c5faa65b9
TJ-Albertson <THOMASJOHNNY77@GMAIL.COM>
2022-08-29 18:58:55 -0500

    box placeholder

--- src/hooks/useMuuri.js
+++ src/hooks/useMuuri.js
@@ -5,7 +5,17 @@ export const useMuuri = (data) => {
   const ref = useRef(null);
 
   useEffect(() => {
-    let grid = new Muuri(ref.current, { dragEnabled: true });
+    let grid = new Muuri(ref.current, { 
+      dragEnabled: true,
+      dragPlaceholder: {
+        enabled: true,
+        createElement(item) {
+          const placeholder = document.createElement("div");
+          placeholder.innerHTML = '<div></div>';
+          return placeholder;
+        }
+      } 
+    });
     return () => grid.destroy();
   }, [data]);
 
