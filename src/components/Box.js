35356a54692c8a623f91e0c0f95f0dc064d72e0b
TJ-Albertson <THOMASJOHNNY77@GMAIL.COM>
2022-09-01 13:01:16 -0500

    context menu

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,5 +1,5 @@
-import React, { useContext, useRef } from "react";
-import { Card, CloseButton, Container, Dropdown, DropdownButton, Image, Row, Col } from "react-bootstrap";
+import React, { useContext, useEffect, useState } from "react";
+import { Card, CloseButton, Container, Image, Row, Col } from "react-bootstrap";
 import download from "downloadjs";
 import { useSortableData } from "./utils";
 
@@ -7,22 +7,30 @@ import Upload from "./Upload";
 
 import { getApi } from "../api/getApi";
 import { useApi } from "../hooks/useApi";
-import { useContextMenu } from "../hooks/useContextMenu"
+import { useContextMenu } from "../hooks/useContextMenu";
 import { postApi } from "../api/postApi";
-import { UserContext } from "./CloudBox"
+import { UserContext } from "./CloudBox";
 
 import "../CSS/Box.css";
-import "../CSS/ContextMenu.css"
+import "../CSS/ContextMenu.css";
 import { Collapse } from "bootstrap";
 
 export default function Box(props) {
+  const [show, setShow] = useState(false);
+  const [points, setPoints] = useState({ x: 0, y: 0 });
+  const [selectedFile, setSelectedFile] = useState({})
 
   const { loading, error, refresh, data } = useApi(
     `http://localhost:5000/getFileList/${props.id}`,
     { dummyData: [] }
   );
-  const signedInUser = useContext(UserContext)
+  const signedInUser = useContext(UserContext);
   const { items, requestSort, sortConfig } = useSortableData(data);
+  useEffect(() => {
+    const handleClick = () => setShow(false);
+    window.addEventListener("click", handleClick);
+    return () => window.removeEventListener("click", handleClick);
+  }, []);
 
   const downloadFile = async (id, path, mimetype) => {
     const result = await getApi(`/downloadFile/${id}`, signedInUser.token);
@@ -38,7 +46,6 @@ export default function Box(props) {
     postApi("/removeBox", data, signedInUser.token).then(props.refresh);
   }
 
-  
   const getClassNamesFor = (name) => {
     if (!sortConfig) {
       return;
@@ -53,73 +60,81 @@ export default function Box(props) {
     { text: "Size", sortBy: "size" },
   ];
 
-
-
-
-
-  //const refArray = useRef([]);;
-
-  const { refArray } = useContextMenu(items)
-
-
-
-
-
-
-
   return (
     <Card className="Box" style={{ width: "40rem", height: "40rem" }}>
       <Card.Header className="d-flex">
-      <Image src={props.picture} roundedCircle="true" style={{ width: "2rem", height: "2rem", marginRight: "10px" }}/>
+        <Image
+          src={props.picture}
+          roundedCircle="true"
+          style={{ width: "2rem", height: "2rem", marginRight: "10px" }}
+        />
         <div className="flex-grow-1">{props.id}</div>
         <CloseButton onClick={() => removeBox()} />
       </Card.Header>
 
       <Container className="overflow-auto" style={{ fontSize: "10px" }}>
-          <Row>
-              {headerArray.map(({ text, sortBy }) => (
-                <Col key={text}>
-                  <button
-                    type="button"
-                    onClick={() => requestSort(sortBy)}
-                    className={getClassNamesFor(sortBy)}
-                  >
-                    {text}
-                  </button>
-                </Col>
-              ))}
+        <Row>
+          {headerArray.map(({ text, sortBy }) => (
+            <Col key={text}>
+              <button
+                type="button"
+                onClick={() => requestSort(sortBy)}
+                className={getClassNamesFor(sortBy)}
+              >
+                {text}
+              </button>
+            </Col>
+          ))}
+        </Row>
+
+        {items.length > 0 ? (
+          items.map(({ _id, name, path, mimeType, size, updatedAt }, i) => (
+            <Row
+              key={i}
+              className="test"
+              onContextMenu={(e) => {
+                e.preventDefault();
+                setShow(true);
+                setSelectedFile({ _id, path, mimeType})
+                setPoints({ x: e.pageX, y: e.pageY });
+              }}
+            >
+              <Col>
+                <i className="bi bi-folder"></i> {name}
+              </Col>
+              <Col>{updatedAt.substring(0, 10)}</Col>
+              <Col>{mimeType}</Col>
+              <Col>{size} bytes</Col>
             </Row>
-            
-            {items.length > 0 ? (
-              items.map(({ _id, name, path, mimeType, size, updatedAt }, i) => (
-                <Row key={_id} >
-                  <Col><i className="bi bi-folder"></i> {name}</Col>
-                  <Col>{updatedAt.substring(0, 10)}</Col>
-                  <Col>{mimeType}</Col>
-                  <Col>{size} bytes</Col>
-                </Row>
-              ))
-            ) : (
-                <div colSpan={5} style={{ fontWeight: "300" }}>
-                  This user has no files uploaded
-                </div>
-            )}
-      {["item-1"].map((item, i) => (
-        <div  key={i}>
-          <div className={item}>Test</div>
-          <div>Test2</div>
-        </div>
-      ))}
-
-      
-
-    </Container>          
+          ))
+        ) : (
+          <div colSpan={5} style={{ fontWeight: "300" }}>
+            This user has no files uploaded
+          </div>
+        )}
+        {show && (
+          <div
+            className="menu"
+            style={{ top: points.y - 125, left: points.x - 25 }}
+          >
+            <ul>
+              <li>
+                <input
+                  type="button"
+                  value="delete"
+                  onClick={() => console.log(selectedFile)}
+                />
+              </li>
+              <li>Pin Message</li>
+              <li>Edit Message</li>
+            </ul>
+          </div>
+        )}
+      </Container>
       <div className="flex-fill"></div>
 
       <Card.Footer>
-        {props.boxEmail == signedInUser.email ? (
-          <Upload></Upload>
-        ) : null}
+        {props.boxEmail == signedInUser.email ? <Upload></Upload> : null}
       </Card.Footer>
     </Card>
   );
