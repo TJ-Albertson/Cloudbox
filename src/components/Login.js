92281a1189c438995a336542d779059bcae79900
TJ-Albertson <THOMASJOHNNY77@GMAIL.COM>
2022-09-21 19:42:13 -0500

    login screen
    background video, replaced login button with svg

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,23 +1,21 @@
 import { React } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
-import gif from "../Videos/hacker.gif"
+import { ReactComponent as Cloud } from "../SVG/cloud.svg";
+import video from "../assets/wispycloudysky.mp4";
 
-export default function Login(props) {
-  const LoginButton = () => {
-    const { loginWithRedirect } = useAuth0();
+import "../CSS/Login.css";
 
-    return <button onClick={() => loginWithRedirect()} style={{color: "red", fontSize: "50px", fontFamily: 'Courier New', fontWeight: "bold"}}>Log In</button>;
-  };
+export default function Login() {
+  const { loginWithRedirect } = useAuth0();
 
   return (
     <div>
+      <video className="background-video" autoPlay muted loop>
+        <source src={video} type="video/mp4" />
+      </video>
 
-      <div style={{color: "red", fontSize: "50px", fontFamily: 'Courier New', fontWeight: "bold", position: "absolute"}} >Welcome to getvirus.net</div>
-      <img src={gif} style={{width: "100%", height: "10%", zIndex: "-2"}}/>
-      <div className="position-absolute top-50 start-50 translate-middle z-index-1">
-        <LoginButton />
-      </div>
+      <Cloud className="cloud-login" onClick={() => loginWithRedirect()} />
     </div>
   );
 }
