cf136adb14b717b7f10562a45efa5af7b5ba2d1a
TJ-Albertson <THOMASJOHNNY77@GMAIL.COM>
2022-10-19 13:13:10 -0500

    added flex styling to settings menu

--- src/components/SettingsModal.js
+++ src/components/SettingsModal.js
@@ -1,18 +1,20 @@
 import React, { useContext } from "react";
 import { Modal, ModalBody, ModalHeader } from "react-bootstrap";
 
+import "../SCSS/SettingsModal.scss"
+
 export default function SettingsModal(props) {
   return (
     <Modal show={props.show} onHide={props.onHide} size="md" centered>
         
-        <ModalHeader>
+        <ModalHeader id="settings-modal-header">
             Settings
         </ModalHeader>
-        <ModalBody>
-            <div>
+        <ModalBody id="settings-modal-body">
+            <div id="settings-modal-tabs">
                 tabs
             </div>
-            <div>
+            <div id="settings-modal-content">
                 content
             </div>
         </ModalBody>
